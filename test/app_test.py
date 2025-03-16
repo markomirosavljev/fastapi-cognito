d25e7aacd05b1ef048048e4e4ca5367666519e27
Marko Mirosavljev <mirosavljevm023@gmail.com>
2025-03-16 11:20:33 +0100

    update tests and setup gh action to run tests

--- test/app_test.py
+++ test/app_test.py
@@ -21,28 +21,29 @@ us_token = boto.generate_access_token(
 def test_no_header():
     resp = t_client.get("/eu")
     assert resp.status_code == 401
+    assert resp.json() == {"detail": "Request does not contain well-formed Cognito JWT"}
 
 def test_default_pool():
     resp = t_client.get("/eu", headers={"Authorization": f"Bearer {eu_token}"})
     assert resp.status_code == 200
-    assert json.loads(resp.content)["message"] == "Hello world"
+    assert resp.json() == {"message": "Hello world"}
 
 def test_selected_pool():
     resp = t_client.get("/us", headers={"Authorization": f"Bearer {us_token}"})
     assert resp.status_code == 200
-    assert json.loads(resp.content)["message"] == "Hello world"
+    assert resp.json() == {"message": "Hello world"}
 
 def test_other_client_issued_token():
     resp = t_client.get("/us", headers={"Authorization": f"Bearer {eu_token}"})
     assert resp.status_code == 401
-    assert json.loads(resp.content)["detail"] == "Error decoding JWT token."
+    resp.json() == {"detail": "Error decoding JWT token."}
 
 def test_optional_no_token():
     resp = t_client.get("/optional")
     assert resp.status_code == 200
-    assert json.loads(resp.content)["message"] == "Hello world"
+    assert resp.json() == {"message": "Hello world"}
 
 def test_optional_no_token():
     resp = t_client.get("/optional", headers={"Authorization": f"Bearer {eu_token}"})
     assert resp.status_code == 200
-    assert json.loads(resp.content)["message"] == "Hello world"
+    assert resp.json() == {"message": "Hello world"}
